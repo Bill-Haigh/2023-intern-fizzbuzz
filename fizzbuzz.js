724b761d95e95f0342a4a83ffae7476b1fa0f590
Bill Haigh <bill.haigh@softwire.com>
2025-07-16 09:20:19 +0100

    FIX/FEAT: changed function to better logic and added Bang

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,14 +1,21 @@
 // This is our main function
 function fizzbuzz() {
     for (let i = 1; i <= 100; i++) {
-        if (i % 3 == 0 && i % 5 == 0) {
-            console.log("FizzBuzz")
-        } else if (i%3 == 0) {
-            console.log("Fizz");
-        } else if (i%5 == 0) {
-            console.log("Buzz");
-        } else {
+        let str = "";
+        if (i%3 == 0) {
+            str = str + "Fizz";
+        }
+        if (i%5 == 0) {
+            str = str + "Buzz";
+        }
+        if (i%7 == 0) {
+            str = str + "Bang"
+        }
+
+        if (!str) {
             console.log(i);
+        } else {
+            console.log(str);
         }
     }
 
